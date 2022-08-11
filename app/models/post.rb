d8cbec7452dd9600cd49f8e943ba58e428258f42
ericmahare <erikmahare@gmail.com>
2022-08-12 01:47:11 +0300

    Add two methods that updates posts counter for a user and another one return 5 most recent comments for a given user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  has_many :likes
+
+  def update_post_counter
+    user.increment!(:postscounter)
+  end
+
+  def recent_comments
+    comments.order('created_at Desc').last(5)
+  end
 end
