c2ffc4a4a56614d06b60a65e04476c3b22be5713
ericmahare <erikmahare@gmail.com>
2022-08-19 03:05:15 +0300

    Add methods to fetch data from the database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,13 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
+    @commets = Comment.where(post_id: @posts.ids[0])
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+    @comments = @post.comments
+  end
 end
