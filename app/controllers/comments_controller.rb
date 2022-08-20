1f0cb645558c1a3853d08db1491cdaa5a3cc42e9
ericmahare <erikmahare@gmail.com>
2022-08-20 17:14:25 +0300

    Refactor comments create method to add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,19 @@
 class CommentsController < ApplicationController
   def new
     @comment = Comment.new
+    respond_to do |format|
+      format.html { render :new, locals: { comment: } }
+    end
   end
 
   def create
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.new(post_id: @post.id, user_id: current_user.id, Text: comment_params)
     @comment.post_id = @post.id
-    @comment.update_comment_counter
     if @comment.save
       redirect_to user_post_path(current_user, @post.id)
     else
-      flash.now[:error] = 'Error: Comment could not be saved. Please try again.'
+      flash.now[:error] = 'Error: Comment could not be saved.'
     end
   end
 
