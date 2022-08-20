f5693060580fcad104752b5b15fffc20177d360b
ericmahare <erikmahare@gmail.com>
2022-08-20 13:28:05 +0300

    Add likes and comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(post_id: @post.id, user_id: current_user.id, Text: comment_params)
+    @comment.post_id = @post.id
+    @comment.update_comment_counter
+    if @comment.save
+      redirect_to user_post_path(current_user, @post.id)
+    else
+      flash.now[:error] = 'Error: Comment could not be saved. Please try again.'
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)[:text]
+  end
+end
