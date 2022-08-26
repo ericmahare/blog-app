9d173122ca33c3a20475ff964c00dacd52188fff
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-08-26 11:05:27 +0200

    Add open letter route config

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,8 @@ Rails.application.routes.draw do
     resources :comments, only: %i[new, create]
     resources :likes, only: %i[create]
   end
+  
+  if Rails.env.development? 
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end 
 end
