f264b909bfe7adbf62c79ec1c0bd87fcc3b9773a
ericmahare <erikmahare@gmail.com>
2022-08-26 11:30:01 +0300

    Add devise user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: true, length: { in: 4..25 }
-  validates :postscounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :postscounter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }, allow_nil: true
   has_many :comments
   has_many :likes
   has_many :posts
@@ -8,3 +12,4 @@ class User < ApplicationRecord
     posts.order('created_at Desc').last(3)
   end
 end
+
