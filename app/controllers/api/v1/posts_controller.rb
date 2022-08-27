cac4fc80d5b44049c9a70d9902d8cb725ccd05f2
ericmahare <erikmahare@gmail.com>
2022-08-27 05:11:41 +0300

    Add api methods for posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,24 @@
 class Api::V1::PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
+    render json: @posts
+  end
+
   def show
     @post = Post.find(params[:id])
     render json: @post
   end
 end
+
+# def index
+#   @user = User.find(params[:user_id])
+#   @posts = @user.posts.includes(:comments)
+#   @current_user = current_user.id
+# end
+
+# def show
+#   @post = Post.find(params[:id])
+#   @user = @post.user
+#   @comments = @post.comments
+# end
