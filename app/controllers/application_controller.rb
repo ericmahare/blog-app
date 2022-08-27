f52b0f54fd0fcd306c99e0d8b14fee187b185761
ericmahare <erikmahare@gmail.com>
2022-08-27 04:58:47 +0300

    Disable CSRF authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery unless: -> { request.format.json? }
+  # before_action :authenticate_user!
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
